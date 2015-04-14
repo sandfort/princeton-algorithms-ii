27b6fc39fdef097531d204232566563745216df1
John Sandfort <jsandfort@amplify.com>
2015-04-14 18:50:31 -0400

    Finishing implementations of WordNet and Outcast. I don't know if these will work, but I submitted them anyway.

--- wordnet/src/Outcast.java
+++ wordnet/src/Outcast.java
@@ -1,3 +1,14 @@
+/**
+ * Outcast.
+ *
+ * Given a list of wordnet nouns A_1 to A_n, which noun is least related to the
+ * others? To identify an outcast, compute the sum of the distances between
+ * each noun and every other one:
+ *
+ *      d_i = dist(A_i, A_1) + dist(A_i, A_2) + , ... + dist(A_i, A_n)
+ *
+ *  and return a noun A_t for which d_t is maximum.
+ */
 public class Outcast {
     private WordNet wordnet;
 
@@ -6,8 +17,25 @@ public class Outcast {
     }
 
     public String outcast(String[]  nouns) {
-        // TODO implement
-        return "";
+        int greatest = 0;
+        String outcast = null;
+
+        for (String noun : nouns) {
+            int dist = 0;
+
+            for (String other : nouns) {
+                if (noun != other) {
+                    dist += wordnet.distance(noun, other);
+                }
+            }
+
+            if (dist > greatest) {
+                greatest = dist;
+                outcast = noun;
+            }
+        }
+
+        return outcast;
     }
 
     public static void main(String[] args) {
--- wordnet/src/WordNet.java
+++ wordnet/src/WordNet.java
@@ -1,7 +1,15 @@
 public class WordNet {
-    Synset[] sets; // array of synsets by synset id
-    int v;  // number of synsets in this wordnet
-    Digraph net; // hypernym directed graph
+    // array of synsets by synset id
+    Synset[] sets;
+
+    // number of synsets in this wordnet
+    int v;
+
+    // hypernym directed graph
+    Digraph net;
+
+    // ST keyed by noun, values are a bag of synsets that the noun belongs to.
+    LinearProbingHashST<String, Bag<Integer>> nouns;
 
     /**
      * Construct a WordNet given a list of synsets and their hypernym
@@ -11,18 +19,25 @@ public class WordNet {
      * @param hypernyms The name of the hypernyms input file.
      */
     public WordNet(String synsets, String hypernyms) {
-        // TODO implement
-        // - read in synsets
-        // - store synsets in a resizing array by id
-        // - initialize digraph with length of synset array
-        // - read in hypernyms
-        // - make edges from synsets to their hypernyms
+        checkForNull(synsets);
+        checkForNull(hypernyms);
 
         this.sets = new Synset[2];
+        this.nouns = new LinearProbingHashST<String, Bag<Integer>>();
 
         In in = new In(synsets);
         while (in.hasNextLine()) {
-            add(new Synset(in.readLine()));
+            Synset synset = new Synset(in.readLine());
+            add(synset);
+            for (String noun : synset.nouns.split(" ")) {
+                if (nouns.contains(noun)) {
+                    nouns.get(noun).add(synset.id);
+                } else {
+                    Bag<Integer> ids = new Bag<Integer>();
+                    ids.add(synset.id);
+                    nouns.put(noun, ids);
+                }
+            }
         }
 
         this.net = new Digraph(v);
@@ -36,6 +51,56 @@ public class WordNet {
                 net.addEdge(id, hypernym);
             }
         }
+
+        if (!(new Topological(net).hasOrder())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Return all nouns in the WordNet.
+     */
+    public Iterable<String> nouns() {
+        return nouns.keys();
+    }
+
+    /**
+     * Is the word a noun in this WordNet?
+     */
+    public boolean isNoun(String word) {
+        checkForNull(word);
+        return nouns.contains(word);
+    }
+
+    /**
+     * Return the distance between nouns A and B.
+     */
+    public int distance(String nounA, String nounB) {
+        checkForNull(nounA);
+        checkForNull(nounB);
+
+        return new SAP(net).length(nouns.get(nounA), nouns.get(nounB));
+    }
+
+    /**
+     * Return a synset that is the common ancestor of nouns A and B in a
+     * shortest ancestral path.
+     */
+    public String sap(String nounA, String nounB) {
+        checkForNull(nounA);
+        checkForNull(nounB);
+
+        Iterable<Integer> synsetsA = nouns.get(nounA);
+        Iterable<Integer> synsetsB = nouns.get(nounB);
+        SAP sap = new SAP(net);
+        return sets[sap.ancestor(synsetsA, synsetsB)].nouns;
+    }
+
+    /**
+     * Unit tests.
+     */
+    public static void main(String[] args) {
+        WordNet wn = new WordNet(args[0], args[1]);
     }
 
     private void resize(int capacity) {
@@ -55,80 +120,34 @@ public class WordNet {
         ++v;
     }
 
-    /**
-     * Return all nouns in the WordNet.
-     */
-    public Iterable<String> nouns() {
-        SET<String> nouns = new SET<String>();
-        for (Synset synset : sets) {
-            nouns = nouns.union(synset.synset);
+    private void checkForNull(Object argument) {
+        if (argument == null) {
+            throw new NullPointerException();
         }
-
-        return nouns;
     }
 
-    /**
-     * Is the word a noun in this WordNet?
-     */
-    public boolean isNoun(String word) {
-        // TODO implement
-        return false;
-    }
-
-    /**
-     * Return the distance between nouns A and B.
-     */
-    public int distance(String nounA, String nounB) {
-        // TODO implement
-        return 0;
-    }
-
-    /**
-     * Return a synset that is the common ancestor of nouns A and B in a
-     * shortest ancestral path.
-     */
-    public String sap(String nounA, String nounB) {
-        // TODO implement
-        return "";
-    }
-
-    /**
-     * Unit tests.
-     */
-    public static void main(String[] args) {
-        WordNet wn = new WordNet(args[0], args[1]);
+    private void checkForCycles(Digraph g) {
     }
 
     static class Synset {
         public int id;
-        public SET<String> synset;
+        public String nouns;
         public String definition;
 
         public Synset(String line) {
             String[] fields = line.split(",");
 
             this.id = Integer.parseInt(fields[0]);
-            String[] nouns = fields[1].split(" ");
-            this.synset = new SET<String>();
-            for (String noun : nouns) {
-                synset.add(noun);
-            }
+            this.nouns = fields[1];
             this.definition = fields[2];
         }
 
         public String toString() {
-            StringBuffer sb = new StringBuffer();
-            sb.append(String.format("id: %d\n", id));
-            sb.append("synonyms: { ");
-            for (String syn : synset) {
-                sb.append(String.format("%s ", syn));
-            }
-            sb.append("}\n");
-            sb.append("def: ");
-            sb.append(definition);
-            sb.append("\n");
-
-            return sb.toString();
+            return new StringBuffer()
+                .append(String.format("id: %d\n", id))
+                .append(String.format("synonyms: { %s }\n", nouns))
+                .append(String.format("def: %s\n", definition))
+                .toString();
         }
     }
 }
