215cdf312e55a56e271ee0c2e3ea6563ac37f1cd
John Sandfort <jsandfort@amplify.com>
2015-04-22 20:16:44 -0400

    Adding implementation of energy() that maybe probably works. (progress step 2)

--- seam-carving/src/SeamCarver.java
+++ seam-carving/src/SeamCarver.java
@@ -1,5 +1,11 @@
+import java.awt.Color;
+
+import static java.lang.Math.*;
+
 public class SeamCarver {
     private Picture picture;
+    private final static int BORDER_ENERGY = 195075;
+
     /**
      * Create a seam carver object based on the given picture.
      */
@@ -30,10 +36,12 @@ public class SeamCarver {
 
     /**
      * The energy of the pixel at column <tt>x</tt> and row <tt>y</tt>.
+     *
+     *  The energy of pixel (x, y) is the square of the x-gradient plus the
+     *  square of the y-gradient.
      */
     public double energy(int x, int y) {
-        // TODO implement
-        return 0.0;
+        return squareOfXGradient(x, y) + squareOfYGradient(x, y);
     }
 
     /**
@@ -67,5 +75,36 @@ public class SeamCarver {
         // TODO implement
         return;
     }
+
+    private double squareOfXGradient(int x, int y) {
+        if (x == 0 || x == picture.width() - 1) {
+            return BORDER_ENERGY;
+        }
+
+        Color left  = picture.get(x - 1, y);
+        Color right = picture.get(x + 1, y);
+
+        double red   = pow(abs(left.getRed()   - right.getRed()),   2);
+        double green = pow(abs(left.getGreen() - right.getGreen()), 2);
+        double blue  = pow(abs(left.getBlue()  - right.getBlue()),  2);
+
+        return red + green + blue;
+    }
+
+    private double squareOfYGradient(int x, int y) {
+        if (y == 0 || y == picture.height() - 1) {
+            return BORDER_ENERGY;
+        }
+
+        Color top    = picture.get(x, y - 1);
+        Color bottom = picture.get(x, y + 1);
+
+        double red   = pow(abs(top.getRed()   - bottom.getRed()),   2);
+        double green = pow(abs(top.getGreen() - bottom.getGreen()), 2);
+        double blue  = pow(abs(top.getBlue()  - bottom.getBlue()),  2);
+
+        return red + green + blue;
+    }
+
 }
 
