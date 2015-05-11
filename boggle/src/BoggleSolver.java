2009e663122a699b7bff4c67d38f36133fde8c58
John Sandfort <jsandfort@amplify.com>
2015-05-11 09:23:54 -0400

    Initial commit for Week 5 - Boggle

---
+++ boggle/src/BoggleSolver.java
@@ -0,0 +1,33 @@
+public class BoggleSolver {
+    /**
+     * Initialize the data structure using the given array of strings as the
+     * dictionary.
+     *
+     * @param dictionary An array of strings consisting only of uppercase
+     * letters A through Z.
+     */
+    public BoggleSolver(String[] dictionary) {
+        // TODO implement
+    }
+
+    /**
+     * Return the set of all valid words in the given Boggle board as an
+     * <tt>Iterable</tt>.
+     */
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        // TODO implement
+        return new Stack<String>();
+    }
+
+    /**
+     * Return the score of the given word if it is in the dictionary, zero
+     * otherwise.
+     *
+     * @param word A word containing only uppercase letters A through Z.
+     */
+    public int scoreOf(String word) {
+        // TODO implement
+        return 0;
+    }
+}
+
