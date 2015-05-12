f30f4be6d25fb46a81768fb53660ad9ef4631463
John Sandfort <jsandfort@amplify.com>
2015-05-12 10:25:09 -0400

    Implementing constructor and scoreOf method.

--- boggle/src/BoggleSolver.java
+++ boggle/src/BoggleSolver.java
@@ -1,4 +1,7 @@
 public class BoggleSolver {
+    // TODO replace this SET with a data structure that supports prefix search
+    private SET<String> dict;
+
     /**
      * Initialize the data structure using the given array of strings as the
      * dictionary.
@@ -7,7 +10,10 @@ public class BoggleSolver {
      * letters A through Z.
      */
     public BoggleSolver(String[] dictionary) {
-        // TODO implement
+        this.dict = new SET<String>();
+        for (String word : dictionary) {
+            this.dict.add(word);
+        }
     }
 
     /**
@@ -26,8 +32,38 @@ public class BoggleSolver {
      * @param word A word containing only uppercase letters A through Z.
      */
     public int scoreOf(String word) {
-        // TODO implement
-        return 0;
+        if (!dict.contains(word)) {
+            return 0;
+        }
+
+        int len = word.length();
+
+        if (len < 3) {
+            return 0;
+        } else if (len < 5) {
+            return 0;
+        } else if (len == 5) {
+            return 3;
+        } else if (len == 6) {
+            return 3;
+        } else if (len == 7) {
+            return 5;
+        }
+
+        return 11;
+    }
+
+    public static void main(String args[]) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
     }
 }
 
