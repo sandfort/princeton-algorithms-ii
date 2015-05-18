637e593ce60dd0dfcefd714e10ed7cfcbb54f637
John Sandfort <jsandfort@amplify.com>
2015-05-18 16:43:11 -0400

    Adding the API for a trie

--- boggle/src/BoggleSolver.java
+++ boggle/src/BoggleSolver.java
@@ -122,5 +122,25 @@ public class BoggleSolver {
             this.j = j;
         }
     }
+
+    private class Trie {
+        public Trie() {
+            // TODO implement
+        }
+
+        public void add(String s) {
+            // TODO implement
+        }
+
+        public boolean containsWord(String s) {
+            // TODO implement
+            return false;
+        }
+
+        public boolean containsPrefix(String s) {
+            // TODO implement
+            return false;
+        }
+    }
 }
 
