25d565ee4c343bf77f0308ed1d08a80120ac0d13
John Sandfort <jsandfort@amplify.com>
2015-05-19 10:03:51 -0400

    Moving Trie to its own file to make it feel more important

---
+++ boggle/src/BoggleTrie.java
@@ -0,0 +1,19 @@
+public class Trie {
+    public Trie() {
+        // TODO implement
+    }
+
+    public void add(String s) {
+        // TODO implement
+    }
+
+    public boolean containsWord(String s) {
+        // TODO implement
+        return false;
+    }
+
+    public boolean containsPrefix(String s) {
+        // TODO implement
+        return false;
+    }
+}
